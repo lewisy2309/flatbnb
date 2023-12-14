bb565ff5d63d08ee7d14462d2b55f639588b62b9
Yvon Lewis <yvonlewisghilbert@gmail.com>
2023-12-14 16:37:51 +0100

    gestion navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AuthenticationService } from './core/services/authentication.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'airbnb.com';
+
+  constructor(
+    public authService: AuthenticationService){
+
+  }
 }
